1ff7ac3a28a30c480eaad95064f9163de4e4c8a6
kat <28567881+hk21702@users.noreply.github.com>
2023-03-29 23:24:53 -0400

    minor changes

--- network/layers/condition_time.py
+++ network/layers/condition_time.py
@@ -3,25 +3,25 @@ from torch import nn
 
 
 class ConditionTime(nn.Module):
-    """Layer to condition the time on the input data"""
+    """Layer to condition time onto a specific step in the sequence"""
 
-    def __init__(self, seq_len_hor: int, dim: int = 2) -> None:
+    def __init__(self, horizon: int, dims: int = 2) -> None:
         super(ConditionTime, self).__init__()
-        self.seq_len_hor = seq_len_hor
-        self.dim = dim
+        self.seq_len_hor = horizon
+        self.dims = dims
 
-    def forward(self, x: torch.Tensor, steps=0):
+    def forward(self, x: torch.Tensor, step=0):
         batch_size, seq_len, channels, height, width = x.size()
 
         # Create the time coordinates
-        times = self.c_time(x, steps,
+        times = self.c_time(x, step,
                             (height, width)).repeat(
             batch_size, seq_len, 1, 1, 1)
 
-        x = torch.cat([x, times], dim=self.dim)
+        x = torch.cat([x, times], dim=self.dims)
 
-        assert x.shape[self.dim] == channels + self.seq_len_hor, \
-            f"Expected {channels + self.seq_len_hor} channels, got {x.shape[self.dim]}"
+        assert x.shape[self.dims] == channels + self.seq_len_hor, \
+            f"Expected {channels + self.seq_len_hor} channels, got {x.shape[self.dims]}"
         return x
 
     def c_time(self, x: torch.Tensor, i: int, size: tuple[int, int]):
